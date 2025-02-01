066966c559710c6cdc5f823d6962e31f3f1a1cfa
Taylor <tbeimel@csumb.edu>
2025-02-01 00:58:21 -0800

    initialized pawn bolo in springboot java, connected to new aws rds db

---
+++ src/test/java/pawnbolo/com/pawnbolo/PawnBoloApplicationTests.java
@@ -0,0 +1,13 @@
+package pawnbolo.com.pawnbolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawnBoloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
